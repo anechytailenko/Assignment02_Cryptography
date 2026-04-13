f71f851cad1d32ce5ea45fbcb7040ae3b49e3432
Illia <iprykhodko@kse.org.ua>
2026-04-13 21:55:21 +0300

    Usage example was added

--- src/include/utils.h
+++ src/include/utils.h
@@ -19,3 +19,14 @@ inline uint64_t strumok_AlphaInvMul(uint64_t w)
 {
     return (w >> 8) ^ strumok_alphainv_mul[w & 0xFF];
 }
+
+inline uint64_t load64LittleEndian(const uint8_t* p) {
+    return static_cast<uint64_t>(p[0]) |
+          (static_cast<uint64_t>(p[1]) << 8) |
+          (static_cast<uint64_t>(p[2]) << 16) |
+          (static_cast<uint64_t>(p[3]) << 24) |
+          (static_cast<uint64_t>(p[4]) << 32) |
+          (static_cast<uint64_t>(p[5]) << 40) |
+          (static_cast<uint64_t>(p[6]) << 48) |
+          (static_cast<uint64_t>(p[7]) << 56);
+}
